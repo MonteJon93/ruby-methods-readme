1c1695a08af6bc40fecc538507a4cb6525131e09
Jonathan Montesdeoca <JMontesdeoca93@gmail.com>
2020-12-09 12:19:21 -0500

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,42 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+# method variable
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+say_hello_world_ten_times
+def greeting # Method Signature
+    puts "Hello World" # Method Body
+  end # Method Closing 
+
+  def say_hello_world_ten_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+  end
+  
+  say_hello_world_ten_times
+  
+  # ... The rest of the program
+  
+  say_hello_world_ten_times 
---
+++ lib/greeting.rb
@@ -0,0 +1,38 @@
+def greeting
+    puts "Hello World"
+  end
+  
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting 
+  
+
+  def greeting
+    puts "Hello World"
+  end
+  
+  greeting
+
+  def greeting
+    puts "Hello World"
+  end 
+
+  def greeting
+    puts "Hello World"
+  end
+  
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting 
+
+  $ ruby greeting.rb
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+$
